d62d1c3e446d17e3bac9501ca72b478694ef46e9
Stef Spee <s.spee@arcady.nl>
2023-11-11 20:13:56 +0100

    Added a new scoreboard model. Added validation attributes to models. Adjusted the form on pages and components to handle attribute validation.

--- src/ScoreBoardHub/Models/ScoreBoardEntry.cs
+++ src/ScoreBoardHub/Models/ScoreBoardEntry.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ScoreBoardHub.Models;
 
-public class ScoreBoardEntry
+public sealed class ScoreBoardEntry
 {
     public string RowKey { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Player name is required")]
     public string PlayerName { get; set; }
+
+    [Required(ErrorMessage = "Score is required")]
+    [Range(0, int.MaxValue, ErrorMessage = "Let's stay positive")]
     public int Score { get; set; }
 }
